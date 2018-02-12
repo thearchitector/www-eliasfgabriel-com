02ce62bb273b747bcd29d6038155a8cefcc66bb9
TheMasterGabriel <energizedpixel@gmail.com>
2018-02-11 22:09:51 -0500

    Add cross-browser support for changing Hello!! message
    Hello Jenna

--- js/main.js
+++ js/main.js
@@ -45,5 +45,17 @@
 		}
 	});
 
+	var hello = $("#hello"),
+		textAnim = new TimelineLite({ onComplete: function(self) { self.restart(); }, onCompleteParams: ["{self}"], paused: true }),
+		messages = ["مرحبا!!", "Salam!!", "Здравейте!!", "হ্যালো!!", "Zdravo!!", "Ahoj!!", "Hallo!!", "Χαίρετε!!", "¡¡Hola!!", "Tere!!", "سلام!!", "Hei!!", "Bonjour!!", "Ola!", "नमस्ते!!", "Sawubona!!", "Բարեւ!!", "Halo!!", "Nnọọ!!", "Halló!!", "Ciao!!", "שלום!!", "こんにちは!!", "안녕하세요!!", "Sveiki!!", "Salama!!", "Здраво!!", "ഹലോ!!", "Bongu!!", "ဟလို!!", "Moni!!", "Cześć!!", "Olá!!", "Buna!!", "Здравствуйте!!", "හෙලෝ!!", "Përshëndetje!!", "Lumela!!", "Hallå!!", "வணக்கம்!!", "హలో!!", "Салом!!", "สวัสดี!!", "Kamusta!!", "Merhaba!!", "ہیلو!!", "Salom!!", "你好!!", "Hello!!"],
+		currentMessage = 0;
+
+	textAnim.to(hello, 1.75, { opacity: 0 }).to(hello, 1.75, { opacity: 1 });;
+	textAnim.add(function()
+	{
+		hello.text(messages[currentMessage]);
+		currentMessage = (currentMessage + 1) % messages.length;
+	}, 1.75);
+	textAnim.play();
 	$('[data-toggle="tooltip"]').tooltip();
 })(jQuery);
